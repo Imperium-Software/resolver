1ddf24da10d1932ec4cb9fbb7edf909ea268c6ff
Regan-Koopmans <regankoopmans@protonmail.com>
2017-07-04 15:47:18 +0200

    Added electron code

--- src/interface/js/controller.js
+++ src/interface/js/controller.js
@@ -1,37 +1,74 @@
 let net = require('net');
+const {dialog} = require('electron').remote;
 
 let HOST = 'rainmaker.wunderground.com';
 let PORT = 23;
 
 // Get pointers to critical elements
-let test_request = document.getElementById('test-request');
-let test_relay = document.getElementById('test-relay');
-let host_field = document.getElementById('host-field')
-let port_field = document.getElementById('port-field')
+// let test_request = document.getElementById('test-request');
+// let test_relay = document.getElementById('test-relay');
+// let host_field = document.getElementById('host-field')
+// let port_field = document.getElementById('port-field')
 
-// Add event listeners
-test_request.addEventListener('click', () => {
-  let client = net.Socket();
+// // Add event listeners
+// test_request.addEventListener('click', () => {
+//   let client = net.Socket();
 
-  HOST = host_field.value;
-  PORT = port_field.value;
+//   HOST = host_field.value;
+//   PORT = port_field.value;
 
-  client.connect(PORT, HOST, function() {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+//   client.connect(PORT, HOST, function() {
+//     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+//     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
 
-    client.write('Hello from Electron interface!\n#\n')
+//     client.write('Hello from Electron interface!\n#\n')
+//   });
+
+//   client.on('data', function(data) {
+
+//     test_relay.value += data;
+//     // Close the client socket completely
+//     client.destroy();
+//   });
+
+//   // Add a 'close' event handler for the client socket
+//   client.on('close', function() {
+//     console.log('Connection closed');
+//   });
+// });
+
+
+
+document.getElementById('open-file').addEventListener('click', () => {
+  dialog.showOpenDialog((filename) => {
+    if (filename === undefined) {
+      console.log('The user did not select a location to open.')
+      return
+    }
+    console.log(filename)
+    fs.readFile(filename[0], 'utf8', (err, data) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      editor.value = data;
+      toggle_hex = false;
+    })
+  })
+})
+
+// Side Nav
+
+ $(".button-collapse").sideNav();
+
+ // Progress Circle
+
+ $('#circle').circleProgress({
+    value: 0.75,
+    size: 80,
+    fill: {
+      gradient: ["purple", "blue"]
+    }
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
   });
-
-  client.on('data', function(data) {
-
-    test_relay.value += data;
-    // Close the client socket completely
-    client.destroy();
-  });
-
-  // Add a 'close' event handler for the client socket
-  client.on('close', function() {
-    console.log('Connection closed');
-  });
-});
