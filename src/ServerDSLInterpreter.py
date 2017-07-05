9783fa517bf792600daa9f76438663cb7b30f6f6
craig95 <u15029779@tuks.co.za>
2017-07-05 23:12:59 +0200

    Added speedtest file to compare try-except and ifs

---
+++ src/ServerDSLInterpreter.py
@@ -0,0 +1,12 @@
+"""
+    Module: Server DSL Interpreter
+    Description: defines a interpreter that can decode requests sent by the client and encode data that needs to be sent to the client.
+"""
+
+
+def decode(data):
+    pass
+
+
+def encode():
+    pass
