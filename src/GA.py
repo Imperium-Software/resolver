543510145e1586bd59572a5f3f9741385a142ac1
DewaldDeJager <dewald.dj@hotmail.com>
2017-07-13 12:03:36 +0200

    Added tabu search with diversification and changed formatting

--- src/GA.py
+++ src/GA.py
@@ -1,26 +1,25 @@
-
 """
     Module: GA
     Description: Defines the genetic algorithm and all the core functionality of it, including crossover and tabu search
 """
 
 from individual import Individual
-import copy
 import random
 import copy
 
+
 class GA:
-    def __init__(self, filename, method):
+    def __init__(self, filename, method=None):
         f = open(filename, "r")
         # Read all the lines from the file that aren't comments
         lines = [line.replace("\n", "") for line in f.readlines() if line[0] != "c" and line.strip() != ""]
         (self.numberOfVariables, self.numberOfClauses) = int(lines[0].split()[2]), int(lines[0].split()[3])
         self.formula = []
         self.method = method
-        
+
         # Initialize tabu to an empty list
         self.tabu = []
-        
+
         # Go through the lines and create numberOfClauses clauses
         line = 1
         # for line in range(1, len(lines)):
@@ -40,6 +39,7 @@ class GA:
                     line += 1
             # clause is now a list of lists, so we need to flatten it and convert it to a list
             self.formula.append(tuple([item for sublist in clause for item in sublist]))
+        self.false_counts = [0 for _ in range(len(self.formula))]
 
     def sat(self, individual, clause):
 
@@ -148,67 +148,67 @@ class GA:
         # Calculates improvement in fitness
         return original_individual_fitness - self.evaluate(new_individual)
 
-    def corrective_clause(self, X, Y):
+    def corrective_clause(self, x, y):
 
         """
             Some docstring
         """
 
-        Z = Individual(self.numberOfVariables, self.method, False)
+        z = Individual(self.numberOfVariables, self.method, False)
         for clause in self.formula:
             best_pos = 0
             best_improvement = 0
-            if not self.sat(X, clause) and not self.sat(Y, clause) and not self.sat_crossover(Z, clause):
+            if not self.sat(x, clause) and not self.sat(y, clause) and not self.sat_crossover(z, clause):
                 for i in range(len(clause)):
-                    current_improvement = self.improvement(X, i) + self.improvement(Y, i)
+                    current_improvement = self.improvement(x, i) + self.improvement(y, i)
                     if current_improvement >= best_improvement:
                         best_improvement = current_improvement
                         best_pos = i
-                Z.set(best_pos, X.get(best_pos))
-                Z.set_defined(best_pos)
-                Z.flip(best_pos)
-                Z.allocate(X, Y)
-        return Z
+                z.set(best_pos, x.get(best_pos))
+                z.set_defined(best_pos)
+                z.flip(best_pos)
+                z.allocate(x, y)
+        return z
 
-    def corrective_clause_with_truth_maintenance(self, X, Y):
+    def corrective_clause_with_truth_maintenance(self, x, y):
 
         """
             See page 9 of the paper
         """
 
-        Z = Individual(self.numberOfVariables, self.method, False)
+        z = Individual(self.numberOfVariables, self.method, False)
         for clause in self.formula:
             best_pos = 0
-            maximum_improvement = self.improvement(X, 0) + self.improvement(Y, 0)
-            if not self.sat(X, clause) and not self.sat(Y, clause) and not self.sat_crossover(Z, clause):
+            maximum_improvement = self.improvement(x, 0) + self.improvement(y, 0)
+            if not self.sat(x, clause) and not self.sat(y, clause) and not self.sat_crossover(z, clause):
                 for i in range(len(clause)):
-                    current_improvement = self.improvement(X, i) + self.improvement(Y, i)
+                    current_improvement = self.improvement(x, i) + self.improvement(y, i)
                     if current_improvement >= maximum_improvement:
                         maximum_improvement = current_improvement
                         best_pos = i
-                Z.set(best_pos, X.get(best_pos))
-                Z.set_defined(best_pos)
-                Z.flip(best_pos)
+                z.set(best_pos, x.get(best_pos))
+                z.set_defined(best_pos)
+                z.flip(best_pos)
 
         # Truth maintenance - See section 4.2 of the paper
         for clause in self.formula:
             best_pos = -1
             minimum_improvement = self.numberOfClauses + 1
-            if self.sat(X, clause) and self.sat(Y, clause) and not self.sat_crossover(Z, clause):
+            if self.sat(x, clause) and self.sat(y, clause) and not self.sat_crossover(z, clause):
                 for i in range(len(clause)):
-                    if X.get(i) == 1 or Y.get(i) == 1:
-                        current_improvement = self.improvement(X, i) + self.improvement(Y, i)
-                        z_new = copy.deepcopy(Z)
+                    if x.get(i) == 1 or y.get(i) == 1:
+                        current_improvement = self.improvement(x, i) + self.improvement(y, i)
+                        z_new = copy.deepcopy(z)
                         z_new.set(best_pos, 1)
                         z_new.set_defined(best_pos, 1)
                         if current_improvement < minimum_improvement and self.sat_crossover(z_new, clause):
                             minimum_improvement = current_improvement
                             best_pos = i
                 if not best_pos == -1:
-                    Z.set(best_pos, 1)
-                    Z.set_defined(best_pos, 1)
-        Z.allocate(X, Y)
-        return Z
+                    z.set(best_pos, 1)
+                    z.set_defined(best_pos, 1)
+        z.allocate(x, y)
+        return z
 
     def standard_tabu(self, individual_in, tabu_size, max_flip, choose_function):
 
@@ -229,54 +229,94 @@ class GA:
             self.tabu = [index] + self.tabu
         return best
 
+    def tabu_with_diversification(self, individual, threshhold, recurse_count, max_false=5):
+
+        """ Tabu search with augmentations to prevent convergence on local maxima. """
+
+        false_clauses = [self.formula[i] for i in range(len(self.formula)) if self.false_counts[i] >= max_false]
+        individual_temp = copy.deepcopy(individual)
+        forbidden_flips = {}
+        for clause in false_clauses:
+            self.check_flip(individual_temp, clause, forbidden_flips, threshhold)
+            for _ in range(recurse_count):
+                non_false_clauses = [self.formula[i] for i in range(len(self.formula))
+                                     if self.sat(individual_temp, clause) and not self.sat(individual, clause)]
+                for nested_clause in non_false_clauses:
+                    self.check_flip(individual_temp, nested_clause, forbidden_flips, threshhold)
+        return individual_temp
+
+    def check_flip(self, individual, clause, iteration_dict, k):
+
+        """ Helper function for tabu_with_diversification. """
+
+        temp_clause = [c for c in clause if c not in iteration_dict.keys()]
+        try:
+            index = max(temp_clause, key=lambda c: self.improvement(individual, c))[0]
+        except ValueError as e:
+            raise e
+        pos = temp_clause[index]
+
+        # Check if pos has been flipped before
+        # flips this one stubborn bit and refuse to flip it back before k flips.
+        if pos in iteration_dict.keys():
+            # Check if pos has been flipped k times and remove it if it has
+            # or increment it if it hasn't
+            if iteration_dict[pos] < k:
+                iteration_dict[pos] = iteration_dict[pos] + 1
+                individual.flip(pos)
+            else:
+                del iteration_dict[pos]
+        else:
+            # Otherwise add pos to the dictionary with an initial count of 1
+            iteration_dict[pos] = 1
+            individual.flip(pos)
 
     def fluerent_and_ferland(self, x, y):
 
         """ Performs the Fluerent & Ferland crossover operator. """
 
-        Z = Individual(self.numberOfVariables, self.method, False)
+        z = Individual(self.numberOfVariables, self.method, False)
         for clause in self.formula:
-            if (self.sat(x, clause) and not self.sat(y, clause)):
+            if self.sat(x, clause) and not self.sat(y, clause):
                 for i in range(len(clause)):
-                    Z.set(i, x(i))
-                    Z.set_defined(i, 1)
-            elif (not self.sat(x, clause) and self.sat(y, clause)):
+                    z.set(i, x(i))
+                    z.set_defined(i, 1)
+            elif not self.sat(x, clause) and self.sat(y, clause):
                 for i in range(len(clause)):
-                    Z.set(i, y(i))
-                    Z.set_defined(i, 1)
-        Z.allocate(x,y)
-        return Z
-    
+                    z.set(i, y(i))
+                    z.set_defined(i, 1)
+        z.allocate(x, y)
+        return z
+
 
 def select(self, population, number_of_individuals):
-    
     """ Selects number_of_individuals from a population. """
-    
-    population.sort(key=evaluate)
+
+    population.sort(key=self.evaluate)
     sub_population = population[0:number_of_individuals]
     child_x = random.choice(sub_population)
     child_y = random.choice(sub_population)
     while child_x == child_y:
         child_x = random.choice(sub_population)
-    return child_x, child_y   
+    return child_x, child_y
 
 
 if __name__ == "__main__":
     # TESTS
     num_fail = 0
     print("Testing SAT instance : f1000")
-    x = GA("../examples/f1000.cnf")
-    if len(x.formula) == x.numberOfClauses:
+    test_individual = GA("../examples/f1000.cnf")
+    if len(test_individual.formula) == test_individual.numberOfClauses:
         print("    len(formula) == numberOfClauses => pass")
     else:
         print("    len(formula) == numberOfClauses => fail")
         num_fail += 1
-    if x.formula[0] == (119, 325, -401):
+    if test_individual.formula[0] == (119, 325, -401):
         print("    formula[0] == (119, 325, -401) => pass")
     else:
         print("    formula[0] == (119, 325, -401) => fail")
         num_fail += 1
-    if x.formula[-1] == (-839, -494, 718):
+    if test_individual.formula[-1] == (-839, -494, 718):
         print("    formula[-1] == (-839, -494, 718) => pass")
     else:
         print("    formula[-1] == (-839, -494, 718) => fail")
@@ -284,18 +324,18 @@ if __name__ == "__main__":
 
     print()
     print("Testing SAT instance : f2000")
-    x = GA("../examples/f2000.cnf")
-    if len(x.formula) == x.numberOfClauses:
+    test_individual = GA("../examples/f2000.cnf")
+    if len(test_individual.formula) == test_individual.numberOfClauses:
         print("    len(formula) == numberOfClauses => pass")
     else:
         print("    len(formula) == numberOfClauses => fail")
         num_fail += 1
-    if x.formula[0] == (1295, 1303, -1372):
+    if test_individual.formula[0] == (1295, 1303, -1372):
         print("    formula[0] == (1295, 1303, -1372) => pass")
     else:
         print("    formula[0] == (1295, 1303, -1372) => fail")
         num_fail += 1
-    if x.formula[-1] == (1952, -450, 952):
+    if test_individual.formula[-1] == (1952, -450, 952):
         print("    formula[-1] == (1952, -450, 952) => pass")
     else:
         print("    formula[-1] == (1952, -450, 952) => fail")
@@ -303,18 +343,18 @@ if __name__ == "__main__":
 
     print()
     print("Testing SAT instance : par16-4-c")
-    x = GA("../examples/par16-4-c.cnf")
-    if len(x.formula) == x.numberOfClauses:
+    test_individual = GA("../examples/par16-4-c.cnf")
+    if len(test_individual.formula) == test_individual.numberOfClauses:
         print("    len(formula) == numberOfClauses => pass")
     else:
         print("    len(formula) == numberOfClauses => fail")
         num_fail += 1
-    if x.formula[0] == (-2, 1):
+    if test_individual.formula[0] == (-2, 1):
         print("    formula[0] == (-2, 1) => pass")
     else:
         print("    formula[0] == (-2, 1) => fail")
         num_fail += 1
-    if x.formula[-1] == (132, 324, -140):
+    if test_individual.formula[-1] == (132, 324, -140):
         print("    formula[-1] == (132, 324, -140) => pass")
     else:
         print("    formula[-1] == (132, 324, -140) => fail")
@@ -322,18 +362,18 @@ if __name__ == "__main__":
 
     print()
     print("Testing SAT instance : par32-5")
-    x = GA("../examples/par32-5.cnf")
-    if len(x.formula) == x.numberOfClauses:
+    test_individual = GA("../examples/par32-5.cnf")
+    if len(test_individual.formula) == test_individual.numberOfClauses:
         print("    len(formula) == numberOfClauses => pass")
     else:
         print("    len(formula) == numberOfClauses => fail")
         num_fail += 1
-    if x.formula[0] == (-1,):
+    if test_individual.formula[0] == (-1,):
         print("    formula[0] == (-1,) => pass")
     else:
         print("    formula[0] == (-1,) => fail")
         num_fail += 1
-    if x.formula[-1] == (-3176,):
+    if test_individual.formula[-1] == (-3176,):
         print("    formula[-1] == (-3176,) => pass")
     else:
         print("    formula[-1] == (-3176,) => fail")
@@ -341,18 +381,18 @@ if __name__ == "__main__":
 
     print()
     print("Testing SAT instance : par32-5-c")
-    x = GA("../examples/par32-5-c.cnf")
-    if len(x.formula) == x.numberOfClauses:
+    test_individual = GA("../examples/par32-5-c.cnf")
+    if len(test_individual.formula) == test_individual.numberOfClauses:
         print("    len(formula) == numberOfClauses => pass")
     else:
         print("    len(formula) == numberOfClauses => fail")
         num_fail += 1
-    if x.formula[0] == (-2, 1):
+    if test_individual.formula[0] == (-2, 1):
         print("    formula[0] == (-2, 1) => pass")
     else:
         print("    formula[0] == (-2, 1) => fail")
         num_fail += 1
-    if x.formula[-1] == (450, -408, 1339):
+    if test_individual.formula[-1] == (450, -408, 1339):
         print("    formula[-1] == (450, -408, 1339) => pass")
     else:
         print("    formula[-1] == (450, -408, 1339) => fail")
