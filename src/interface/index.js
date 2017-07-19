af3fcfde3b27aefa6331e2db8ae656a1d04b1e16
Regan-Koopmans <regankoopmans@protonmail.com>
2017-07-19 09:30:21 +0200

    Custom scrollbar, started playground

--- src/interface/index.js
+++ src/interface/index.js
@@ -8,7 +8,9 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, 
+                            backgroundColor: '#f0f3f4', 
+                            show:false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -26,6 +28,10 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
+  })
+
+  win.once("ready-to-show", () => {
+    win.show()
   })
 }
 
