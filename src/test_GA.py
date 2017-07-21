e2f6f90d0b41868facb461df6703200e49995ce9
Regan-Koopmans <regankoopmans@protonmail.com>
2017-07-21 22:13:00 +0200

    Added empty testing functions for GA in test_GA.py

---
+++ src/test_GA.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+
+class TestGA(TestCase):
+    def test_sat(self):
+        self.fail()
+
+    def test_sat_crossover(self):
+        self.fail()
+
+    def test_evaluate(self):
+        self.fail()
+
+    def test_improvement(self):
+        self.fail()
+
+    def test_corrective_clause(self):
+        self.fail()
+
+    def test_corrective_clause_with_truth_maintenance(self):
+        self.fail()
+
+    def test_standard_tabu_choose(self):
+        self.fail()
+
+    def test_standard_tabu(self):
+        self.fail()
+
+    def test_choose_rvcf(self):
+        self.fail()
+
+    def test_weight(self):
+        self.fail()
+
+    def test_degree(self):
+        self.fail()
+
+    def test_tabu_with_diversification(self):
+        self.fail()
+
+    def test_check_flip(self):
+        self.fail()
+
+    def test_fluerent_and_ferland(self):
+        self.fail()
+
+    def test_select(self):
+        self.fail()
+
+    def test_create_population(self):
+        self.fail()
+
+    def test_is_satisfied(self):
+        self.fail()
+
+    def test_replace(self):
+        self.fail()
+
+    def test_gasat(self):
+        self.fail()
