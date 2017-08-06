b1c42260e78985c0b289ea16e1430f25fb634249
Regan <u15043143@tuks.co.za>
2017-08-06 09:40:17 +0200

    Added test-case for Factory, removed redundant Individual comments.

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,11 +1,15 @@
 from unittest import TestCase
+from SATSolver.individual import Factory
 
 
 class TestFactory(TestCase):
-
     """
     Test class for Factory
     """
-
+    
     def test_create(self):
-        self.fail()
+        factory = Factory()
+        population = factory.create(10, None, 50)
+        self.assertEqual(50, len(population))
+        for individual in population:
+            self.assertEqual(individual.length, 10)
