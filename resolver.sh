f3fc2acb3434a49628d84021f86da81ffb45963a
Regan-Koopmans <regankoopmans@protonmail.com>
2017-10-08 10:54:59 +0200

    Changed start script

--- resolver.sh
+++ resolver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-electron SATSolver/interface > SATSolver/interface/log & 
-python3 SATSolver/main.py
+python3 SATSolver/main.py & electron SATSolver/interface > SATSolver/interface/log 
