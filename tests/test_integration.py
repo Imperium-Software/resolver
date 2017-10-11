35df9caf04bd7e0c634e1b8533d9d896b6c78110
Regan-Koopmans <regankoopmans@protonmail.com>
2017-10-11 07:25:36 +0200

    Fixing docs directory, removed BitVector from dependencies.txt

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.insert(0, '../SATSolver/')
+import socket
 from unittest import TestCase
 from multiprocessing import Process
 from SATController import  SATController
@@ -12,4 +13,10 @@ class IntegrationTests(TestCase):
         controller = SATController.instance()
         controller.server_thread = Process(target=SATServer, args=('localhost', 3000, decode))
         controller.server_thread.start()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('localhost', 3000))
+        sock.close()
+
         controller.server_thread.terminate()
+        self.assertEqual(1, 1)
