ed33b11379876d70256cdde2ae2ac2950accf43c
DewaldDeJager <dewald.dj@hotmail.com>
2017-10-19 04:19:13 +0200

    Add setup.py file

---
+++ SATSolver/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Resolver',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'SATSolver'},
+    url='http://imperium-engineering.com',
+    license='MIT',
+    author='Imperium Engineering',
+    author_email='imperium@dearvolt.com',
+    description='',
+    install_requires=[
+        'bitarray',
+        'pympler'
+      ]
+)
